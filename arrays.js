7890c632b4e7b04b49c6c83008c1741022bfc77a
Your Name <you@example.com>
2020-01-30 16:11:38 -0800

    print indices

---
+++ arrays.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  // Replace this with your code
+  for (const index in items){
+      console.log(`${items[index]} ${index}`);
+  }
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
